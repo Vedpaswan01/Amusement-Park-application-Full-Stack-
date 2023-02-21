42ea7eb1fb6c61c4e79e86f95f1e1a1e2acaa12e
Vedprakash Paswan <vedpaswan01@gmail.com>
2023-02-21 21:11:45 +0530

    folder structure

---
+++ Amusement_park/src/main/java/com/FunXtreme/AmusementParkApplication.java
@@ -0,0 +1,13 @@
+package com.FunXtreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusementParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmusementParkApplication.class, args);
+	}
+
+}
---
+++ Amusement_park/src/test/java/com/FunXtreme/AmusementParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FunXtreme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
